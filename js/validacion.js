1a7191f9ae832701abdc3d340a4f72298130e965
juan18506 <juan185067@gmail.com>
2023-08-11 13:37:53 -0300

    entregable

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,36 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const btn = document.querySelector('button')
+
+const nombre = document.getElementById('nombre')
+const apellido = document.getElementById('apellido')
+const email = document.getElementById('email')
+const password1 = document.getElementById('password1')
+const password2 = document.getElementById('password2')
+const terminos = document.getElementById('terminos')
+
+btn.addEventListener('click', () => {
+    if (!nombre.value || !apellido.value || !email.value || !password1.value || !password2.value) {
+        showAlertError()
+        return
+    }
+
+    if (password1.value.length < 6) {
+        showAlertError()
+        return
+    }
+
+    if (password1.value !== password2.value) {
+        showAlertError()
+        return
+    }
+
+    if (!terminos.checked) {
+        showAlertError()
+        return
+    }
+
+    showAlertSuccess()
+})
